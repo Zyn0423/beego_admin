918a1fb53184b4facfa952fa5f80af221efa777d
Zxn <zxnyn0423@163.com>
2020-09-14 04:41:00 +0800

    post案例

--- controllers/default.go
+++ controllers/default.go
@@ -11,5 +11,15 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.Data["key_"] = "goadmin"
+	c.TplName = "index_1.html"
+}
+
+type IndexController struct {
+	 beego.Controller
+}
+
+func (c *IndexController) Post()  {
+	c.Data["key_"] = "修改成功"
+	c.TplName = "index_1.html"
 }
