d54d80ce72de702c2327cf3ae5c6f8fff0c7d3ad
Zxn <zxnyn0423@163.com>
2020-09-14 18:24:54 +0800

    ORM-Mysql 增删改查操作

--- routers/router.go
+++ routers/router.go
@@ -10,5 +10,10 @@ func init() {
 	//beego.Router("/index", &controllers.IndexController{})
 	//beego.Router("/index", &controllers.IndexController{},"get:ShowGet;post:Post")  // TODO 指定路由
 	//beego.Router("/index/?:id", &controllers.IndexController{},"get:ShowGet;post:Post")  // TODO 正则路由
-	beego.Router("/index/mysql", &controllers.MysqlController{},"get:ShowMysql")  //
+	//beego.Router("/index/mysql", &controllers.MysqlController{},"get:ShowMysql")  //
+	beego.Router("/index/ormsql", &controllers.OrmMysqlController{},"get:ShowGoMysql")  //  创建表
+	beego.Router("/index/insert", &controllers.OrmMysqlController{},"get:ShowInsertMysql")  // 插入
+	beego.Router("/index/find", &controllers.OrmMysqlController{},"get:ShowFindMysql")  // 查
+	beego.Router("/index/updata", &controllers.OrmMysqlController{},"get:ShowupdataMysql")  // 更新
+	beego.Router("/index/delete", &controllers.OrmMysqlController{},"get:ShowDeleteMysql")  // 删除
 }
