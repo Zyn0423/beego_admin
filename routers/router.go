0b341c30ad106a1ec26651bab1e541583151e930
Zxn <zxnyn0423@163.com>
2020-09-14 17:17:29 +0800

    gomsql 操作方法

--- routers/router.go
+++ routers/router.go
@@ -9,5 +9,6 @@ func init() {
     beego.Router("/", &controllers.MainController{})
 	//beego.Router("/index", &controllers.IndexController{})
 	//beego.Router("/index", &controllers.IndexController{},"get:ShowGet;post:Post")  // TODO 指定路由
-	beego.Router("/index/?:id", &controllers.IndexController{},"get:ShowGet;post:Post")  // TODO 正则路由
+	//beego.Router("/index/?:id", &controllers.IndexController{},"get:ShowGet;post:Post")  // TODO 正则路由
+	beego.Router("/index/mysql", &controllers.MysqlController{},"get:ShowMysql")  //
 }
